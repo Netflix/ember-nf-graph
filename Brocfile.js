f19abec5c65b9e4b2d1966473a9c364a40e504cc
Ben Lesh <blesh@netflix.com>
2014-06-24 13:35:29 -0700

    fixed style building, but there's still a loop.

--- Brocfile.js
+++ Brocfile.js
@@ -3,41 +3,26 @@ var mergeTrees = require('broccoli-merge-trees');
 var scssCompile = require('broccoli-sass');
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var exportTree = require('broccoli-export-tree');
+var pickFiles = require('broccoli-static-compiler');
 
 var appTree    = mergeTrees(['app', 'app-addon'], { overwrite: true });
-var vendorTree = mergeTrees(['vendor', 'vendor-addon']);
 
-var app = new EmberApp({
-	trees: {
-		vendor: vendorTree,
-		app: appTree
-	}
-});
+var compiledCss = scssCompile(['styles-addon'], 'main.scss', 'ember-cli-ember-dvc.css');
 
-var cssTree = scssCompile(['styles-addon'], 'main.scss', 'ember-cli-ember-dvc.css');
-// var outputCss = (cssTree, {
-// 	destDir: '/'
-// });
-
-app.import('vendor/d3/d3.js');
-app.import('vendor-addon/ember-cli-ember-dvc/ember-cli-ember-dvc.css');
-
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
-
-var wat = exportTree(cssTree, {
+var exportedCss = exportTree(compiledCss, {
 	destDir: 'vendor-addon/ember-cli-ember-dvc', 
 	clobber: true
 });
 
-module.exports = mergeTrees([app.toTree(), wat]);
+var app = new EmberApp({
+	trees: {
+		vendor: mergeTrees(['vendor', 'vendor-addon']),
+		app: appTree
+	}
+});
+
+
+app.import('vendor/d3/d3.js');
+app.import('vendor/ember-cli-ember-dvc/ember-cli-ember-dvc.css');
+
+module.exports = mergeTrees([exportedCss, app.toTree()]);
