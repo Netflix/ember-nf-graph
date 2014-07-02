80ce207f02ebd4ce635f5e6bc5f93fc49acf0dcd
Ben Lesh <blesh@netflix.com>
2014-07-02 10:50:35 -0700

    fixed some dependencies, added column sorts

--- app-addon/components/nf-table.js
+++ app-addon/components/nf-table.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
+import multiSort from '../utils/multi-sort';
+import { SORT_NONE, SORTTYPE_SINGLE, SORTTYPE_MULTI } from '../utils/constants';
 
 export default Ember.Component.extend({
 	isDataTable: true,
 	tagName: 'table',
 	rows: null,
+	
+	sortType: SORTTYPE_SINGLE,
 
 	_columns: null,
 	columns: function(name, value) {
@@ -34,17 +38,39 @@ export default Ember.Component.extend({
 		this.set('hasRendered', true);
 	},
 
+	sortedRows: function(){
+		var sort = this.get('columns').filter(function(col) {
+			return col.get('sortDirection') && col.get('sortBy');
+		}).map(function(col) {
+			return {
+				by: col.get('sortBy').replace(/^row\./, ''),
+				direction: col.get('sortDirection')
+			};
+		});
+
+		var rowsCopy = this.get('rows').slice();
+		multiSort(rowsCopy, sort);
+		return rowsCopy;
+	}.property('rows.[]', 'columns.@each.sortDirection'),
+
 	parentController: Ember.computed.alias('templateData.view.controller'),
 
 	actions: {
 		sort: function(sortedColumn) {
 			var columns = this.get('columns');
-			columns.filter(function(column) {
-				return column !== sortedColumn;
-			}).forEach(function(column) {
-				column.unsort();
-			});
-			sortedColumn.sortBy();
+			var sortType = this.get('sortType');
+			var currentSortDir = sortedColumn.get('sortDirection');
+
+			if(sortType === SORTTYPE_SINGLE) {
+				columns.forEach(function(col) {
+					col.set('sortDirection', SORT_NONE);
+				});
+				sortedColumn.set('sortDirection', [1, 1, -1][currentSortDir + 1]);
+			}
+
+			if(sortType === SORTTYPE_MULTI) {
+				sortedColumn.set('sortDirection', [0, 1, -1][currentSortDir + 1]);
+			}
 		}
 	}
 });
--- app-addon/utils/constants.js
+++ app-addon/utils/constants.js
@@ -1,3 +1,5 @@
 export var SORT_ASCENDING = 1;
 export var SORT_DESCENDING = -1;
-export var SORT_NONE = 0;
+export var SORT_NONE = 0; 
+export var SORTTYPE_SINGLE = 'single';
+export var SORTTYPE_MULTI = 'multi';
