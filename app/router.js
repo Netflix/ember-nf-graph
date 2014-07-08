b2a669946d8bef6c3a342f7c01ef24da221216e5
Ben Lesh <blesh@netflix.com>
2014-07-08 16:27:41 -0700

    bars begin

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
 	this.resource('nf-graph', function(){
 		this.route('/');
+		this.route('/nf-bars');
 	});
 
 	this.resource('nf-table', function(){
