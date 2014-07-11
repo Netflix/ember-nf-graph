7d3b8b9a98cdf34595392dfae5c0372a7b21a25e
Ben Lesh <blesh@netflix.com>
2014-07-10 17:22:56 -0700

    fix offset on lines in ordinal scale

--- app-addon/mixins/graph-line-utils.js
+++ app-addon/mixins/graph-line-utils.js
@@ -1,15 +1,19 @@
 import Ember from 'ember';
 export default Ember.Mixin.create({
-	createLineFn: function(xscale, yscale, interpolate){ 
+	createLineFn: function(xScale, yScale, interpolate){ 
 		var interp = interpolate || 'linear';
+
+        var xMod = xScale.rangeBand ? xScale.rangeBand() / 2 : 0;
+        var yMod = yScale.rangeBand ? yScale.rangeBand() / 2 : 0;
+
         return function(data) {
         	if(!data) {
         		return 'M0,0';
         	}
         	
         	return d3.svg.line()
-    	      .x(function (d) { return xscale(d[0]); })
-    	      .y(function (d) { return yscale(d[1]); })
+    	      .x(function (d) { return xScale(d[0]) + xMod; })
+    	      .y(function (d) { return yScale(d[1]) + yMod; })
     	      .interpolate(interp)(data);
         };
 	}
