e3917e894d5bba24053a473f1f40152d32efea20
Ben Lesh <blesh@netflix.com>
2014-07-14 16:47:20 -0700

    add range marker

--- app-addon/components/nf-range-marker.js
+++ app-addon/components/nf-range-marker.js
@@ -5,33 +5,51 @@ import HasGraphParent from '../mixins/graph-has-graph-parent';
 export default Ember.Component.extend(HasGraphParent, {
 	tagName: 'g',
 
+	attributeBindings: ['transform'],
+
 	classNames: ['nf-range-marker'],
 
 	xMin: 0,
 	xMax: 0,
-	topMargin: 15,
+	marginTop: 10,
+	marginBottom: 3,
 	height: 10,
 
-  labelPadding: 3,
+  x: property('xMin', 'graph.xScale', function(xMin, xScale) {
+  	return xScale(xMin);
+  }),
 
-	x: property('xMin', 'graph.xScale', function(xMin, xScale) {
-		return xScale(xMin);
-	}),
+  width: property('xMin', 'xMax', 'graph.xScale', function(xMin, xMax, xScale) {
+  	return xScale(xMax - xMin);
+  }),
 
-	y: property('graphHeight', 'prevMarker.y', 'prevMarker.topMargin', 'prevMarker.topPadding', function(){
-		return 0;
-	}),
+  y: property('container.orient', 'prevMarker.bottom', 'prevMarker.y', 'graph.graphHeight', 'totalHeight', function(orient, prevBottom, prevY, graphHeight, totalHeight){
+  	prevBottom = prevBottom || 0;
 
-	width: property('xMin', 'xMax', 'graph.xScale', function(xMin, xMax, xScale) {
-		return xScale(xMax - xMin);
-	}),
+  	if(orient === 'bottom') {
+  		return (prevY || graphHeight) - totalHeight;
+  	}
 
-	labelTransform: property('x', 'y', 'labelPadding',
-		function(x, y, labelPadding) {
-			y -= labelPadding;
-			return 'translate(%@ %@)'.fmt(x, y);
-		}
-	),
+  	if(orient === 'top') {
+  		return prevBottom;
+  	}
+  }),
+
+  totalHeight: property('height', 'marginTop', 'marginBottom', function(height, marginTop, marginBottom) {
+  	return height + marginTop + marginBottom;
+  }),
+
+  bottom: property('y', 'totalHeight', function(y, totalHeight) {
+  	return y + totalHeight;
+  }),
+
+  transform: property('y', function(y) {
+  	return 'translate(0 %@)'.fmt(y);
+  }),
+
+  labelTransform: property('x', function(x){
+  	return 'translate(%@ 0)'.fmt(x);
+  }),
 
 	_setup: function(){
 		var container = this.nearestWithProperty('isRangeMarkerContainer');
