e5ccfa90f3b3b2d3fbe9b3cfa335d34a2c9936ac
Ben Lesh <blesh@netflix.com>
2014-07-16 15:37:19 -0700

    switching branches for a bug fix

--- app-addon/components/nf-inline-bar.js
+++ app-addon/components/nf-inline-bar.js
@@ -1,5 +1,43 @@
 import Ember from 'ember';
+import { property } from '../utils/computed-property-helpers';
+import parsePropertyExpr from '../utils/parse-property-expression';
+
+var computedAlias = Ember.computed.alias;
 
 export default Ember.Component.extend({
-	tagName: 'g'
+	tagName: 'rect',
+
+	attributeBindings: ['x', 'y', 'width', 'height', 'dataValue'],
+
+	classNames: ['nf-inline-bar'],
+	
+	width: 0,
+
+	height: 0,
+
+	x: 0,
+
+	y: 0,
+
+	yprop: 'y',
+
+	data: null,
+
+	getYProp: property('yprop', function(yprop) {
+		return parsePropertyExpr(yprop);
+	}),
+
+	dataValue: property('getYProp', 'data', function(getYProp, data) {
+		return getYProp(data);
+	}),
+
+	_register: function(){
+		var graph = this.nearestWithProperty('isInlineGraph');
+		graph.registerGraphic(this);
+		this.set('graph', graph);
+	}.on('init'),
+
+	_unregister: function(){
+		this.get('graph').unregisterGraphic(this);
+	}.on('willDestroyElement')
 });
--- app-addon/components/nf-inline-graph.js
+++ app-addon/components/nf-inline-graph.js
@@ -4,12 +4,17 @@ import { property } from '../utils/computed-property-helpers';
 
 export default Ember.Component.extend({
 	tagName: 'div',
+	
 	isInlineGraph: true,
-	className: ['nf-inline-graph'],
+
+	classNames: ['nf-inline-graph'],
 
 	width: 400,
+	
 	collapsedHeight: 100,
+	
 	expandedHeight: 500,
+	
 	expand: false,
 
 	height: property('expand', 'collapsedHeight', 'expandedHeight', function(expand, collapsedHeight, expandedHeight) {
@@ -38,5 +43,16 @@ export default Ember.Component.extend({
 
 	unregisterGraphic: function(graphic) {
 		this.get('graphics').removeObject(graphic);
-	}
+	},
+
+	_d3Setup: function() {
+		var div = d3.select(this.$()[0]);
+		var svg = div.selectAll('svg');
+		var inlineBars = div.selectAll('svg > .nf-inline-bar');
+
+		this.set('_div', div);
+		this.set('_svg', svg);
+		this.set('_inlineBars', inlineBars);
+		
+	}.on('didInsertElement'),
 });
