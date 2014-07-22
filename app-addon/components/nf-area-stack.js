7e21bd4736c4b3956f1f3fce6e3c09e72d5df85c
Ben Lesh <blesh@netflix.com>
2014-07-22 11:43:44 -0700

    add nf-area-stack docs

--- app-addon/components/nf-area-stack.js
+++ app-addon/components/nf-area-stack.js
@@ -1,24 +1,79 @@
 import Ember from 'ember';
 
+/**
+ * A component for grouping and stacking `nf-area` components in an `nf-graph`.
+ * 
+ * This component looks at the order of the `nf-area` components underneath it 
+ * and uses the ydata of the next sibling `nf-area` component to determine the bottom 
+ * of each `nf-area` components path to be drawn.
+ *
+ * @module ember-cli-ember-dvc
+ * @class nf-area-stack
+ * @example
+ *
+ *  		{{#nf-graph width=300 height=100}}
+ *  			{{#nf-graph-content}}
+ *  				{{#nf-area-stack}}
+ *  					{{nf-area data=myData xprop="time" yprop="high"}}
+ *  					{{nf-area data=myData xprop="time" yprop="med"}}
+ *  					{{nf-area data=myData xprop="time" yprop="low"}}
+ *  				{{/nf-area-stack}}
+ *  			{{/nf-graph-content}}
+ *  		{{/nf-graph}}
+ * 
+ */
 export default Ember.Component.extend({
 	tagName: 'g',
+
+	/**
+	 * Used by `nf-area` to identify an area stack parent
+	 * @property isAreaStack
+	 * @type Boolean
+	 * @default true
+	 * @readonly
+	 */
 	isAreaStack: true,
 
+	/**
+	 * The collection of `nf-area` components under this stack.
+	 * @property areas
+	 * @type Array
+	 * @readonly
+	 */
 	areas: function(){
 		return [];
 	}.property(),
 
+	/**
+	 * Registers an area component with this stack. Also links areas to one
+	 * another by setting `nextArea` on each area component.
+	 * @function registerArea
+	 * @param area {Ember.Component} The area component to register.
+	 */
 	registerArea: function(area) {
 		var areas = this.get('areas');
 		var last = areas[areas.length - 1];
 		if(last) {
 			last.set('nextArea', area);
+			this.set('prevArea', last);
 		}
 		
 		areas.pushObject(area);
 	},
 
+	/**
+	 * Unregisters an area component from this stack. Also updates next
+	 * and previous links.
+	 * @function unregisterArea
+	 * @param area {Ember.Component} the area to unregister
+	 */
 	unregisterArea: function(area) {
+		var prev = area.get('prevArea');
+		var next = area.get('nextArea');
+
+		prev.set('nextArea', next);
+		next.set('prevArea', prev);
+
 		this.get('areas').removeObject(area);
 	},
 });
--- docs/api.js
+++ docs/api.js
@@ -1,6 +1,7 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
+        "nf-area-stack",
         "nf-graph",
         "nf-x-axis",
         "nf-y-axis"
@@ -12,7 +13,7 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "ember-cli-ember-dvc",
             "name": "ember-cli-ember-dvc",
-            "description": "A container component for building complex Cartesian graphs.\n\n## Minimal example\n\n      {{#nf-graph width=100 height=50}}\n        {{#nf-graph-content}}\n          {{nf-line data=lineData xprop=\"foo\" yprop=\"bar\"}}\n        {{/nf-graph-content}}\n      {{/nf-graph}}\n\nThe above will create a simple 100x50 graph, with no axes, and a single line\nplotting the data it finds on each object in the array `lineData` at properties\n`foo` and `bar` for x and y values respectively.\n\n## More advanced example\n\n      {{#nf-graph width=500 height=300}}\n        {{#nf-x-axis height=\"50\"}}\n          <text>{{tick.value}}</text>\n        {{/nf-x-axis}}\n  \n        {{#nf-y-axis width=\"120\"}}\n          <text>{{tick.value}}</text>\n        {{/nf-y-axis}}\n  \n        {{#nf-graph-content}}\n          {{nf-line data=lineData xprop=\"foo\" yprop=\"bar\"}}\n        {{/nf-graph-content}}\n      {{/nf-graph}}\n\nThe above example will create a 500x300 graph with both axes visible. The graph will not \nrender either axis unless its component is present."
+            "description": "A component for grouping and stacking `nf-area` components in an `nf-graph`.\n\nThis component looks at the order of the `nf-area` components underneath it \nand uses the ydata of the next sibling `nf-area` component to determine the bottom \nof each `nf-area` components path to be drawn."
         }
     ]
 } };
