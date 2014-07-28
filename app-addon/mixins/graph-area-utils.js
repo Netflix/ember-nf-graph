ddb1ca08595e955289646acdd3ec177262f42b40
Ben Lesh <blesh@netflix.com>
2014-07-28 09:56:45 -0700

    fixes to area and line - fixed null or empty data coming into nf-area - fixes for ordinal scales with nf-area (Issue #1) - fixes for NaN returns from scale functions (Issue #11)

--- app-addon/mixins/graph-area-utils.js
+++ app-addon/mixins/graph-area-utils.js
@@ -19,16 +19,26 @@ export default Ember.Mixin.create({
     @return {Function} a function that when called will create SVG path data.
   */
 	createAreaFn: function(xScale, yScale, interpolator) {
-    return d3.svg.area()
+      var interp = interpolator || 'linear';
+      var xMod = xScale.rangeBand ? xScale.rangeBand() / 2 : 0;
+      var yMod = yScale.rangeBand ? yScale.rangeBand() / 2 : 0;
+
+      return function(data) {
+        if(!data || data.length === 0) {
+          return 'M0,0';
+        }
+
+        return d3.svg.area()
           .x(function (d) {
-            return xScale(d[0]);
+            return (xScale(d[0]) || 0) + xMod;
           })
           .y0(function (d) {
-            return yScale(d[1]);
+            return (yScale(d[1]) || 0) + yMod;
           })
           .y1(function (d) {
-            return yScale(d[2]);
+            return (yScale(d[2]) || 0) + yMod;
           })
-          .interpolate(interpolator);
+          .interpolate(interp)(data);
+      };
 	}
 });
