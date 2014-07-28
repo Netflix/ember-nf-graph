041418b051119150ef012d7f7425bb90d2f31366
Ben Lesh <blesh@netflix.com>
2014-07-28 14:07:40 -0700

    update documentation

--- app-addon/components/nf-graph-content.js
+++ app-addon/components/nf-graph-content.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
 /**
- * Container component for graphics to display in `nf-graph`. Represents
- * the area where the graphics, such as lines will display.
- *
- * Exists for layout purposes.
- * @namespace components
- * @class nf-graph-content
- */
+  Container component for graphics to display in `nf-graph`. Represents
+  the area where the graphics, such as lines will display.
+  
+  Exists for layout purposes.
+  @namespace components
+  @class nf-graph-content
+*/  
 export default Ember.Component.extend({
   tagName: 'g',
   
@@ -15,16 +15,55 @@ export default Ember.Component.extend({
 
   attributeBindings: ['transform'],
 
+  /**
+    The SVG transform for positioning the graph content
+    @property transform
+    @type String
+    @readonly
+  */
   transform: function(){
     return 'translate(%@ %@)'.fmt(this.get('x'), this.get('y'));
   }.property('x', 'y'),
 
+  /**
+    The x position of the graph content
+    @property x
+    @type Number
+    @readonly
+  */
   x: Ember.computed.alias('graph.graphX'),
+
+  /**
+    The calculated y position of the graph content
+    @property y
+    @type Number
+    @readonly
+  */
   y: Ember.computed.alias('graph.graphY'),
+
+  /**
+    The calculated width of the graph content
+    @property width
+    @type Number
+    @readonly
+  */
   width: Ember.computed.alias('graph.graphWidth'),
+
+  /**
+    The calculated height of the graph content.
+    @property height
+    @type Number
+    @readonly
+  */
   height: Ember.computed.alias('graph.graphHeight'),
 
 
+  /**
+    A array containing models to render the grid lanes
+    @property gridLanes
+    @type Array
+    @readonly
+  */
   gridLanes: function () {
     var ticks = this.get('graph.yAxis.ticks');
     var width = this.get('width');
@@ -58,6 +97,12 @@ export default Ember.Component.extend({
     return lanes;
   }.property('graph.yAxis.ticks', 'width', 'height'),
 
+  /**
+    An array containing models to render fret lines
+    @property frets
+    @type Array
+    @readonly
+  */
   frets: Ember.computed.alias('graph.xAxis.ticks'),
 
   _setup: function(){
