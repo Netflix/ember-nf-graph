3b6664123fc5ca25d8fae94cc57174c56a59f650
Ben Lesh <blesh@netflix.com>
2015-01-28 09:34:48 -0800

    add proper addon imports

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-ember-dvc'
+  name: 'ember-cli-ember-dvc',
+
+  included: function(app) {
+  	this._super.included(app);
+
+  	app.import('vendor/ember-cli-ember-dvc/ember-cli-ember-dvc.css');
+  }
 };
