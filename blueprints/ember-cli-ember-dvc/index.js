fdb7cf7df0222218e517fa610e915008641af1f8
Ben Lesh <blesh@netflix.com>
2015-02-02 16:00:55 -0800

    remove rxjs and rx-ember

--- blueprints/ember-cli-ember-dvc/index.js
+++ blueprints/ember-cli-ember-dvc/index.js
@@ -4,9 +4,7 @@ module.exports = {
 
   afterInstall: function() {
   	return this.addBowerPackagesToProject([
-  		{ name: 'd3', target: '~3.5.3' },
-  		{ name: 'rxjs', target: '~2.3.24' },
-  		{ name: 'rx-ember', target: '~0.2.2' }
+  		{ name: 'd3', target: '~3.5.3' }
   	]);
   }
 };
--- index.js
+++ index.js
@@ -8,7 +8,6 @@ module.exports = {
   	this._super.included(app);
 
   	app.import('vendor/ember-cli-ember-dvc/ember-cli-ember-dvc.css');
-  	app.import(app.bowerDirectory + '/rx-ember/dist/rx-ember.js');
-  	app.import(app.bowerDirectory + '/rxjs/dist/rx.all.js');
+  	app.import(app.bowerDirectory + '/d3/d3.js');
   }
 };
