bfa3bf9c52ab439dc5feabd71c2f17b5651bb408
Jason Mitchell <jasonmit@yahoo-inc.com>
2015-03-30 09:49:37 -0700

    Renaming blueprint so that it execs on ember:install

---
+++ blueprints/ember-cli-nf-graph/index.js
@@ -0,0 +1,10 @@
+/* globals module */
+module.exports = {
+	normalizeEntityName: function() {},
+
+	afterInstall: function() {
+		return this.addBowerPackagesToProject([
+			{ name: 'd3', target: '~3.5.3' }
+		]);
+	}
+};
