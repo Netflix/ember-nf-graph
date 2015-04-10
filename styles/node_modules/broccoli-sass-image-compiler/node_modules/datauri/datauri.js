ee9f65430e8bd808fa736c1fce90142231be419e
Ben Lesh <blesh@netflix.com>
2015-04-10 16:32:00 -0700

    add rule for style node_modules

--- styles/node_modules/broccoli-sass-image-compiler/node_modules/datauri/datauri.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./lib/datauri/module');
--- styles/node_modules/broccoli-sass-image-compiler/node_modules/datauri/lib/helper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// http://www.nczonline.net/blog/2012/12/11/are-your-mixins-ecmascript-5-compatible/
-exports.mixin = function (receiver, supplier) {
-    Object.keys(supplier).forEach(function (property) {
-        Object.defineProperty(receiver, property, Object.getOwnPropertyDescriptor(supplier, property));
-    });
-};
