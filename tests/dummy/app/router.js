ec9526f43737fc3e5f0d0019347e56c5e9b2da60
Felix Faerber <ffaerber@gmail.com>
2015-11-05 13:12:46 +0100

    remove resource from router

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('nf-graph', function(){
+  this.route('nf-graph', function(){
     this.route('/');
     this.route('nf-bars');
   });
