6521390b99d93950c0a4f40abe1487a61738dea6
Zach Wentz <zkwentz@gmail.com>
2015-12-07 11:48:26 -0800

    fix another nearest with prop bug

--- addon/polyfills/nearest-with-property.js
+++ addon/polyfills/nearest-with-property.js
@@ -7,13 +7,9 @@ import Ember from 'ember';
  @namespace shims
  */
 export default function nearestWithProperty(prop, parent) {
-  nearestWithProp = parent.get(prop);
-  if (!nearestWithProp) {
-    while(true) {
-      parent = parent.get('parentView');
-      if (typeof parent === 'undefined' || !parent) { return; }
-      if (parent[prop]) { return parent; }
-    }
+  while(true) {
+    parent = parent.get('parentView');
+    if (typeof parent === 'undefined' || !parent) { return; }
+    if (parent[prop]) { return parent; }
   }
-  return nearestWithProp;
 }
