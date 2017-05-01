1ddd891b579cf146806deddafd1cc71f12810216
Offir Golan <offirgolan@gmail.com>
2017-05-01 11:40:06 -0700

    Upgrade dependencies

--- blueprints/ember-nf-graph/index.js
+++ blueprints/ember-nf-graph/index.js
@@ -4,8 +4,8 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'd3', target: '~3.5.3' },
-      { name: 'rxjs', target: '~2.3.24' }
+      { name: 'd3', target: '~3.5.17' },
+      { name: 'rxjs', target: '~4.1.0' }
     ]);
   }
 };
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
 
 module.exports = {
   name: 'ember-nf-graph',
 
   included: function(app) {
-    this._super.included(app);
+    this._super.included.apply(this, arguments);
 
-    // app.import('vendor/ember-nf-graph/ember-nf-graph.css');
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
     app.import(app.bowerDirectory + '/d3/d3.js');
-    // app.import('vendor/ember-nf-graph/ember-jquery-svg-class-patch.js');
     app.import(app.bowerDirectory + '/rxjs/dist/rx.all.js');
   }
 };
